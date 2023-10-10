80944ab521f99ac1d8ade817c079660b54abebff
Lewey <dlmelchor12@gmail.com>
2023-10-10 04:57:23 -0700

    more gun sounds
    -more gun sounds

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ class Crosshair(pg.sprite.Sprite):
         self.image = pg.image.load(picture_path)
         self.rect = self.image.get_rect()
 
-
-        self.gunshot1 = pg.mixer.Sound('sounds/gunshot_1.mp3')
-        self.gunshot2 = pg.mixer.Sound('sounds/gunshot_2.mp3')
+        self.gunshot1 = pg.mixer.Sound('sounds/gunshot1.mp3')
+        self.gunshot2 = pg.mixer.Sound('sounds/gunshot2.mp3')
+        self.gunshot3 = pg.mixer.Sound('sounds/gunshot3.mp3')
+        self.gunshot4 = pg.mixer.Sound('sounds/gunshot4.mp3')
+        self.gunshot5 = pg.mixer.Sound('sounds/gunshot5.mp3')
+        self.gunshot6 = pg.mixer.Sound('sounds/gunshot6.mp3')
 
     def shoot(self):
-        self.gunshots = [self.gunshot1, self.gunshot2]
+        self.gunshots = [self.gunshot1, self.gunshot2, self.gunshot3,
+                        self.gunshot4, self.gunshot5, self.gunshot6]
         gunshot = random.choice(self.gunshots)
 
         if gunshot == self.gunshot1:
             self.gunshot1.play()
-        else:
+        elif gunshot == self.gunshot2:
             self.gunshot2.play()
+        elif gunshot == self.gunshot3:
+            self.gunshot3.play()
+        elif gunshot == self.gunshot4:
+            self.gunshot4.play()
+        elif gunshot == self.gunshot5:
+            self.gunshot5.play()
+        elif gunshot == self.gunshot6:
+            self.gunshot6.play()
 
         pg.sprite.spritecollide(crosshair, target_group, True)
 
@@ -45,6 +57,7 @@ class GameState():
                 sys.exit()
 
             if event.type == pg.MOUSEBUTTONDOWN:
+                crosshair.shoot()
                 self.state = 'main_game'
 
         pg.display.flip()
@@ -106,7 +119,8 @@ crosshair_group.add(crosshair)
 # Targets
 target_group = pg.sprite.Group()
 for target in range(20):
-    new_target = Target('sprites/target_1.png', random.randrange(0, screen_width), random.randrange(0, screen_height))
+    new_target = Target('sprites/target_1.png', random.randrange(0, screen_width),
+                        random.randrange(0, screen_height))
     target_group.add(new_target)
 
 while True:
