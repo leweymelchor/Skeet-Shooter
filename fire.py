586228797045b693286d4ee88e74dd5de378bc23
Lewey <dlmelchor12@gmail.com>
2023-10-10 22:23:24 -0700

    working intro
    - gun fire randomized
    - shattering targets
    - gun visible
    - intro screen

---
+++ fire.py
@@ -0,0 +1,30 @@
+# Player Class
+import pygame as pg
+
+class Fire(pg.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pg.image.load('sprites/shot1.png'))
+        self.sprites.append(pg.image.load('sprites/shot2.png'))
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg, sys
 import random
+from fire import Fire
 
 # Sprites
 class Crosshair(pg.sprite.Sprite):
@@ -33,11 +34,23 @@ class Crosshair(pg.sprite.Sprite):
         elif gunshot == self.gunshot6:
             self.gunshot6.play()
 
-        pg.sprite.spritecollide(crosshair, target_group, True)
+        if pg.sprite.spritecollide(crosshair, target_group, True):
+            pg.time.delay(25)
+            new_target.hit()
+            fire_blast.draw(screen)
+            fire_blast.update()
 
     def update(self):
         self.rect.center = pg.mouse.get_pos()
 
+class Gun(pg.sprite.Sprite):
+    def __init__(self, picture_path):
+        super().__init__()
+        self.image = pg.image.load(picture_path)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.topleft = pg.mouse.get_pos()
 
 class Target(pg.sprite.Sprite):
     def __init__(self, picture_path, pos_x, pos_y):
@@ -45,6 +58,30 @@ class Target(pg.sprite.Sprite):
         self.image = pg.image.load(picture_path)
         self.rect = self.image.get_rect()
         self.rect.center = [pos_x, pos_y]
+
+        self.hit1 = pg.mixer.Sound('sounds/hit1.wav')
+        self.hit2 = pg.mixer.Sound('sounds/hit2.wav')
+        self.hit3 = pg.mixer.Sound('sounds/hit3.wav')
+        self.hit4 = pg.mixer.Sound('sounds/hit4.wav')
+        self.hit5 = pg.mixer.Sound('sounds/hit5.wav')
+        self.hit6 = pg.mixer.Sound('sounds/hit6.wav')
+
+    def hit(self):
+        self.hits = [self.hit1, self.hit2, self.hit3, self.hit4, self.hit5, self.hit6]
+        hit_sound = random.choice(self.hits)
+
+        if hit_sound == self.hit1:
+            self.hit1.play()
+        elif hit_sound == self.hit2:
+            self.hit2.play()
+        elif hit_sound == self.hit3:
+            self.hit3.play()
+        elif hit_sound == self.hit4:
+            self.hit4.play()
+        elif hit_sound == self.hit5:
+            self.hit5.play()
+        elif hit_sound == self.hit6:
+            self.hit6.play()
 
 class GameState():
     def __init__(self):
@@ -58,6 +95,7 @@ class GameState():
 
             if event.type == pg.MOUSEBUTTONDOWN:
                 crosshair.shoot()
+                new_target.hit()
                 self.state = 'main_game'
 
         pg.display.flip()
@@ -69,6 +107,8 @@ class GameState():
 
         crosshair_group.draw(screen)
         crosshair_group.update()
+        gun_group.draw(screen)
+        gun_group.update()
 
     def main_game(self):
         for event in pg.event.get():
@@ -82,9 +122,16 @@ class GameState():
         for x in range(0, 1280, 230):
             for y in range(0, 720, 256):
                 screen.blit(background, (x, y))
+
         target_group.draw(screen)
+        print(target_group)
         crosshair_group.draw(screen)
         crosshair_group.update()
+        gun_group.draw(screen)
+        gun_group.update()
+
+        if not target_group:
+            self.state = 'intro'
 
         pg.display.flip()
 
@@ -99,6 +146,7 @@ class GameState():
 pg.init()
 clock = pg.time.Clock()
 game_state = GameState()
+
 # Screen
 screen_width = 1280
 screen_height = 720
@@ -116,12 +164,21 @@ crosshair = Crosshair('sprites/crosshair.png')
 crosshair_group = pg.sprite.Group()
 crosshair_group.add(crosshair)
 
+# Gun
+gun = Gun('sprites/rifle.png')
+gun_group = pg.sprite.Group()
+gun_group.add(gun)
+
 # Targets
 target_group = pg.sprite.Group()
 for target in range(20):
     new_target = Target('sprites/target_1.png', random.randrange(0, screen_width),
                         random.randrange(0, screen_height))
     target_group.add(new_target)
+
+fire_blast = pg.sprite.Group()
+fire = Fire((screen_width // 2) + 50, (screen_height // 2) - 50)
+fire_blast.add(fire)
 
 while True:
     game_state.state_manager()
